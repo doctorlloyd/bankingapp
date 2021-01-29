a51d70c534cb3c59cf6728df8c14dbc4433a8b02
Doctor <doctor@datawizzards.io>
2021-01-29 12:41:46 +0200

    UPDATE:: Added buttons for optional processes

--- lib/ui/views/account_detail.dart
+++ lib/ui/views/account_detail.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 
 class AccountView extends StatelessWidget {
   final Account account;
+
   AccountView({this.account});
 
   @override
@@ -21,7 +22,7 @@ class AccountView extends StatelessWidget {
         elevation: 0.0,
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -29,7 +30,47 @@ class AccountView extends StatelessWidget {
             Text(account.name, style: headerStyle),
             UIHelper.verticalSpaceMedium(),
             Text(account.accountNumber),
-            // Comments(post.id)
+            UIHelper.verticalSpaceLarge(),
+            Center(
+              child: RaisedButton.icon(
+                onPressed: null,
+                label: Text('Add Account'),
+                icon: Icon(
+                  Icons.account_box,
+                  color: headerColor,
+                  size: 24.0,
+                  semanticLabel: 'You want to Add Account, try me!!',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  RaisedButton.icon(
+                    onPressed: null,
+                    label: Text('Withdraw'),
+                    icon: Icon(
+                      Icons.exposure_minus_1,
+                      color: headerColor,
+                      size: 24.0,
+                      semanticLabel: 'You want to Withdraw, try me!!',
+                    ),
+                  ),
+                  RaisedButton.icon(
+                    onPressed: null,
+                    label: Text('Deposit'),
+                    icon: Icon(
+                      Icons.account_balance_wallet_outlined,
+                      color: headerColor,
+                      size: 24.0,
+                      semanticLabel: 'You want to Deposit, try me!!',
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
